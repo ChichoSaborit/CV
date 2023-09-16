1ab13bf4d6dc7ef0c6ba374d58e269169b1f8f94
Chicho <jsaborit@iibintech.com.ar>
2023-09-16 18:14:31 -0300

    Primer intento

---
+++ comportamiento.js
@@ -0,0 +1,451 @@
+function mostrar(id){
+    document.getElementById(id).style.display = "block";
+};
+
+function ocultar(id){
+    document.getElementById(id).style.display = "none";
+};
+
+document.getElementById("boton_inicio").addEventListener("click", function(){
+    mostrar("inicio");
+    mostrar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_datos").addEventListener("click", function(){
+    mostrar("datos");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_formacion").addEventListener("click", function(){
+    mostrar("formacion");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_tema").addEventListener("click", function(){
+    mostrar("tema");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_publicaciones").addEventListener("click", function(){
+    mostrar("publicaciones");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_presentaciones").addEventListener("click", function(){
+    mostrar("presentaciones");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_asistencias").addEventListener("click", function(){
+    mostrar("asistencias");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_participaciones").addEventListener("click", function(){
+    mostrar("participaciones");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_colaboraciones").addEventListener("click", function(){
+    mostrar("colaboraciones");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_experiencias").addEventListener("click", function(){
+    mostrar("experiencias");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_docencia").addEventListener("click", function(){
+    mostrar("docencia");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_gestion").addEventListener("click", function(){
+    mostrar("gestion");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_cursos").addEventListener("click", function(){
+    mostrar("cursos");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_becas").addEventListener("click", function(){
+    mostrar("becas");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_idiomas").addEventListener("click", function(){
+    mostrar("idiomas");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_informatica").addEventListener("click", function(){
+    mostrar("informatica");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("distinciones");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_distinciones").addEventListener("click", function(){
+    mostrar("distinciones");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("contacto");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_contacto").addEventListener("click", function(){
+    mostrar("contacto");
+    ocultar("inicio");
+    ocultar("gif");
+    ocultar("datos");
+    ocultar("formacion");
+    ocultar("tema");
+    ocultar("publicaciones");
+    ocultar("presentaciones");
+    ocultar("asistencias");
+    ocultar("participaciones");
+    ocultar("colaboraciones");
+    ocultar("experiencias");
+    ocultar("docencia");
+    ocultar("gestion");
+    ocultar("cursos");
+    ocultar("becas");
+    ocultar("idiomas");
+    ocultar("informatica");
+    ocultar("distinciones");
+    ocultar("botonera2");
+    ocultar("botonera3");
+});
+
+document.getElementById("boton_produccion").addEventListener("click", function(){
+    mostrar("botonera2");
+});
+
+document.getElementById("boton_conocimientos").addEventListener("click", function(){
+    mostrar("botonera3");
+});
+
+document.getElementById("boton_enviar").addEventListener('click', function(){
+    document.getElementById("contacto").innerHTML = "<h2>Muchas gracias por su mensaje!</h2>"
+})
